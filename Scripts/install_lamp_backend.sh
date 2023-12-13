b741c6e53a06935797a07d489e75533418c18758
Ubuntu <ubuntu@ip-172-31-92-246.ec2.internal>
2023-12-13 13:27:44 +0000

    error en env arreglao

--- Scripts/install_lamp_backend.sh
+++ Scripts/install_lamp_backend.sh
@@ -8,3 +8,13 @@ apt update
 
 source .env
 
+apt-get install mysql-server -y
+
+sed -i "s/127.0.0.1/$moodle_db_host/" /etc/mysql/mysql.conf.d/mysqld.cnf
+
+sudo mysql -u root <<< "DROP USER IF EXISTS '$moodle_db_user'@'$moodle_db_host';"
+sudo mysql -u root <<< "CREATE USER '$moodle_db_user'@'$moodle_db_host' IDENTIFIED BY '$moodle_db_pass';"
+sudo mysql -u root <<< "GRANT ALL PRIVILEGES ON \`$moodle_db_name\`.* TO '$moodle_db_user'@'$moodle_db_host';"
+sudo mysql -u root <<< "FLUSH PRIVILEGES;"
+
+systemctl restart mysql
