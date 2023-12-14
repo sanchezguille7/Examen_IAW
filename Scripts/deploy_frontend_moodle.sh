cc44c89036f000c112e7f66ed76a740acc016308
Ubuntu <ubuntu@ip-172-31-92-246.ec2.internal>
2023-12-14 23:19:29 +0000

    Casi listo

--- Scripts/deploy_frontend_moodle.sh
+++ Scripts/deploy_frontend_moodle.sh
@@ -4,6 +4,10 @@ set -ex
 
 source .env
 
+sudo a2enmod rewrite
+
+sudo systemctl restart apache2
+
 sudo rm -rf /tmp/v4.3.1.zip
 
 wget https://github.com/moodle/moodle/archive/refs/tags/v4.3.1.zip -P /tmp
@@ -14,17 +18,23 @@ sudo apt install unzip -y
 
 unzip /tmp/v4.3.1.zip -d /var/www/html/
 
-sudo chown www-data:www-data /var/www/html/* -R
+sudo mv moodle-4.3.1/* /var/www/html/
+
+sudo chown -R www-data:www-data /var/www/html/
+
+sudo chmod -R 755 /var/www/html/
 
 sudo rm -rf /var/www/moodledata
 
-mkdir /var/www/moodledata
+sudo mkdir /var/www/moodledata
 
-chmod 0777 /var/www/moodledata
+sudo chown -R www-data:www-data /var/www/moodledata
+
+sudo chmod -R 777 /var/www/moodledata
 
 sudo chmod +x /var/www/html/moodle-4.3.1/admin/cli/install.php
 
-sudo -u www-data php /var/www/html/moodle-4.3.1/admin/cli/install.php \
+ sudo -u www-data /usr/bin/php /var/www/html/admin/cli/install.php \
     --lang=$moodle_lang \
     --wwwroot=$moodle_wwwroot \
     --dataroot=$moodle_dataroot \
