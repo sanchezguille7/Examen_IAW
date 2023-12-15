97e4c5193e8ea53f65de6883ba27a06c59a55f7c
root <root@ip-172-31-90-210.ec2.internal>
2023-12-15 13:24:35 +0000

    kk

--- Scripts/deploy_backend.sh
+++ Scripts/deploy_backend.sh
@@ -6,8 +6,8 @@ source .env
 
 mysql -u root <<< "DROP DATABASE IF EXISTS $moodle_db_name"
 mysql -u root <<< "CREATE DATABASE $moodle_db_name"
-mysql -u root <<< "DROP USER IF EXISTS '$moodle_db_user'@'%'"
-mysql -u root <<< "CREATE USER '$moodle_db_user'@'%' IDENTIFIED BY '$moodle_db_pass'"
-mysql -u root <<< "GRANT ALL PRIVILEGES ON $moodle_db_name.* TO '$moodle_db_user'@'%'"
+mysql -u root <<< "DROP USER IF EXISTS '$moodle_db_user'@'$ip_cliente_mysql'"
+mysql -u root <<< "CREATE USER '$moodle_db_user'@'$ip_cliente_mysql' IDENTIFIED BY '$moodle_db_pass'"
+mysql -u root <<< "GRANT ALL PRIVILEGES ON $moodle_db_name.* TO '$moodle_db_user'@'$ip_cliente_mysql'"
 
 systemctl restart mysql
